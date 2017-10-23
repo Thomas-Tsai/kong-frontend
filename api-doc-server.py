95e3d06763f4dedf895ff456cee4d54c37857939
Thomas Tsai <thomas@nchc.org.tw>
2017-10-23 16:07:48 +0000

    update and add css

--- api-doc-server.py
+++ api-doc-server.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 SQLITE_DB_PATH = 'apidoc.db'
 APIList = []
 # The Endpoint of base URL
-kongurl = "http://10.12.0.10:8001"
-#kongurl = "http://172.17.0.3:8001"
+#kongurl = "http://10.12.0.10:8001"
+kongurl = "http://172.17.0.3:8001"
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -30,10 +30,14 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-
 class apiForm(Form):
     apiid = HiddenField('apiid')
     version = StringField('Version', [validators.Length(min=1, max=64)])
+    name = StringField('name', [validators.Length(min=1, max=64)])
+    shortname = StringField('short name', [validators.Length(min=1, max=64)])
+    method = StringField('method', [validators.Length(min=1, max=64)])
+    uri = StringField('uri', [validators.Length(min=1, max=64)])
+    host = StringField('host', [validators.Length(min=1, max=64)])
     group = StringField('Group', [validators.Length(min=1, max=64)])
     description = TextAreaField('Description', [validators.optional(), validators.length(max=2048)])
     params = TextAreaField('Params', [validators.optional(), validators.length(max=2048)])
@@ -47,8 +51,11 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-def runApi(url):
-    myResponse = requests.get(url, verify=True)
+def runApi(url, method='get', data=''):
+    if method == 'get':
+        myResponse = requests.get(url, verify=True)
+    elif method == 'post':
+        myResponse = requests.post(url, data, verify=True) 
     print (myResponse.status_code)
     
     # For successful API call, response code will be 200 (OK)
@@ -78,48 +85,58 @@ def runApi(url):
 def index():
     listurl = kongurl + "/apis"
     kongData = runApi(listurl)
-    print(kongData)
     APIList = []
     apiData = kongData['data']
     for api in apiData:
         apiID = api['id']
+        apiName = api['name']
         db = get_db()
         db.row_factory = dict_factory
         apiShortName=''
         apiDesc=''
         apiGroup=''
-        #(apiShortName, apiDesc, apiGroup) = db.execute("""select (name, description, group_name) from apis where apiID=?;""" , (apiID)).fetchone()
-        cursor = db.execute("""select name, description, group_name from apis where apiID=?;""" , (apiID,))
+        cursor = db.execute("""select shortname, desc, apigroup from apis where apiID=?;""" , (apiID,))
         for row in cursor:
-            apiShortName = row['name']
-            apiDesc = row['description']
-            apiGroup = row['group_name']
+            apiShortName = row['shortname']
+            apiDesc = row['desc']
+            apiGroup = row['apigroup']
         
-        apiName = api['name']
-        apiListData = {'apiname':apiName, 'apiid':apiID, 'apishortname':apiShortName, 'apidesc':apiDesc, 'apigroup':apiGroup}
+        apiListData = {'name':apiName, 'kongid':apiID, 'shortname':apiShortName, 'apidesc':apiDesc, 'apigroup':apiGroup, 'apiid':apiID}
         APIList.append(apiListData)
     return render_template('index.html', api_list = APIList)
 
-@app.route('/addAPI', methods=['GET', 'POST'])
-def addAPI():
-    params = None
-    form = apiForm(request.form)
+@app.route("/api")
+@app.route("/displayAPI")
+def displayAPI():
     if request.method == 'GET':
         apiId = request.args.get('apiid', '000')
-        form.apiid = apiId
+        apidata = {'name':''}
         return render_template(
-            'addAPI.html',
-            form=form
+            'displayAPI.html', api = apidata
             )
-    if request.method == 'POST':
-        apiID = request.form.get('apiid', "000")
-        apiName = request.form.get('name', "")
-        apiDesc = request.form.get('desc', "")
-        apiGroup = '0'
+    return redirect(url_for('index'))
+
+@app.route('/addAPI', methods=['GET', 'POST'])
+def addAPI():
+    form = apiForm(request.form)
+    if request.method == 'POST' and form.validate():
+        #print("form data: %s", form.version.data)
+        addApiUrl = kongurl+"/apis"
+        upstreamUrl = "http://"+form.host.data+"/"+form.uri.data
+        addApiData = {'name':form.name.data, 'hosts':form.host.data, 'upstream_url':upstreamUrl, 'uris':form.uri.data}
+        api = runApi(addApiUrl, 'post', addApiData)
+        print("API ADDED %s", api)
+        apiID = api['id']
+        #apiName = api['name']
+        shortName = request.form.get('shortname', "")
+        apiDesc = request.form.get('desccription', "")
+        apiGroup = request.form.get('group', "")
         db = get_db()
-        db.execute("""insert into apis(name, description, apiID, group_name) values (?,?,?,?);""" , (apiName, apiDesc, apiID, apiGroup))
+        db.execute("""insert into apis(shortname, desc, apiid, apigroup) values (?,?,?,?);""" , (shortName, apiDesc, apiID, apiGroup))
         db.commit()
         return redirect(url_for('index'))
+    
+    return render_template('addAPI.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
--- create-db.sql
+++ create-db.sql
@@ -1,7 +1,11 @@
 CREATE TABLE apis (
     id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    apiID TEXT NOT NULL,
-    group_name TEXT
+    apiid TEXT NOT NULL,
+    shortname TEXT NOT NULL,
+    desc TEXT,
+    params TEXT,
+    apigroup TEXT,
+    example TEXT,
+    success TEXT,
+    error TEXT
 );
