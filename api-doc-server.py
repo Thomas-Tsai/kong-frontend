1d2778eca7e921a5cc6c834551a98b7940915fa8
Thomas Tsai <thomas@nchc.org.tw>
2017-10-26 16:54:49 +0000

    support add, update, delete

--- api-doc-server.py
+++ api-doc-server.py
@@ -52,10 +52,17 @@ def close_connection(exception):
         db.close()
 
 def runApi(url, method='get', data=''):
+    print('method: ', method)
     if method == 'get':
         myResponse = requests.get(url, verify=True)
     elif method == 'post':
         myResponse = requests.post(url, data, verify=True) 
+    elif method == 'patch':
+        print("xx",url)
+        print("xxdata",data)
+        myResponse = requests.patch(url, data, verify=True) 
+    elif method == 'delete':
+        myResponse = requests.delete(url, verify=True) 
     print (myResponse.status_code)
     
     # For successful API call, response code will be 200 (OK)
@@ -66,8 +73,10 @@ def runApi(url, method='get', data=''):
         # Loading the response data into a dict variable
         # json.loads takes in only binary or string variables so using content to fetch binary content
         # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
-        jData = json.loads(myResponse.text)
-    
+        try:
+            jData = json.loads(myResponse.text)
+        except:
+            jData = {}
         #print("The response contains {0} properties".format(len(jData)))
         #print("\n")
         #for key in jData:
@@ -95,7 +104,7 @@ def index():
         apiShortName=''
         apiDesc=''
         apiGroup=''
-        cursor = db.execute("""select shortname, desc, apigroup from apis where apiID=?;""" , (apiID,))
+        cursor = db.execute("""select shortname, desc, apigroup from apis where apiid=?;""" , (apiID,))
         for row in cursor:
             apiShortName = row['shortname']
             apiDesc = row['desc']
@@ -105,12 +114,80 @@ def index():
         APIList.append(apiListData)
     return render_template('index.html', api_list = APIList)
 
+@app.route("/updateAPI")
+def updateAPI():
+    form = apiForm(request.form)
+    if request.method == 'GET':
+        apiId = request.args.get('apiid')
+        apidata = runApi(kongurl+'/apis/'+apiId)
+        db = get_db()
+        db.row_factory = dict_factory
+        cursor = db.execute("""select shortname, desc, params, apigroup, example, success, error from apis where apiid=?;""" , (apiId,))
+        for row in cursor:
+            apidata['shortname'] = row['shortname']
+            apidata['desc'] = row['desc']
+            apidata['params'] = row['params']
+            apidata['apigroup'] = row['apigroup']
+            apidata['example'] = row['example']
+            apidata['success'] = row['success']
+            apidata['error'] = row['error']
+        form.apiid.data = apiId
+        form.version.data = ''
+        form.name.data = apidata['name']
+        form.shortname.data = apidata['shortname']
+        #form.method.data = apidata['method']
+        form.uri.data = apidata['uris'][0]
+        form.host.data = apidata['hosts'][0]
+        form.group.data = apidata['apigroup']
+        form.description.data = apidata['desc']
+        form.params.data = apidata['params']
+        form.example.data = apidata['example']
+        form.success.data = apidata['success']
+        form.error.data = apidata['error']
+    return render_template('updateAPI.html', form=form)
+
+@app.route("/saveAPI", methods=['POST'])
+def saveAPI():
+    form = apiForm(request.form)
+    if request.method == 'POST' and form.validate():
+        apiid = form.apiid.data
+        updateApiUrl = kongurl+"/apis/"+apiid
+        upstreamUrl = "http://"+form.host.data+"/"+form.uri.data
+        updateApiData = {'name':form.name.data, 'hosts':form.host.data, 'upstream_url':upstreamUrl, 'uris':form.uri.data}
+        api = runApi(updateApiUrl, 'patch', updateApiData)
+        shortName = form.shortname.data
+        apiDesc = form.description.data
+        apiGroup = form.group.data
+        db = get_db()
+        db.execute("""update apis set shortname=?, desc=?, apigroup=? where apiid=?""" , (shortName, apiDesc, apiGroup, apiid))
+        db.commit()
+        return redirect(url_for('index'))
+    
+@app.route("/deleteAPI")
+def deleteAPI():
+    if request.method == 'GET':
+        apiId = request.args.get('apiid')
+        apidata = runApi(kongurl+'/apis/'+apiId, 'delete')
+    return redirect(url_for('index'))
+
 @app.route("/api")
 @app.route("/displayAPI")
 def displayAPI():
     if request.method == 'GET':
         apiId = request.args.get('apiid', '000')
-        apidata = {'name':''}
+        apidata = runApi(kongurl+'/apis/'+apiId)
+        db = get_db()
+        db.row_factory = dict_factory
+        cursor = db.execute("""select shortname, desc, params, apigroup, example, success, error from apis where apiid=?;""" , (apiId,))
+        for row in cursor:
+            apidata['shortname'] = row['shortname']
+            apidata['desc'] = row['desc']
+            apidata['params'] = row['params']
+            apidata['apigroup'] = row['apigroup']
+            apidata['example'] = row['example']
+            apidata['success'] = row['success']
+            apidata['error'] = row['error']
+        
         return render_template(
             'displayAPI.html', api = apidata
             )
